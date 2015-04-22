17d37e742d05caa5af0a031112e87e1f356f5a6d
Denis Lamtsov <denisla@parallels.com>
2015-04-22 13:16:25 -0700

    Beautify log output

--- debug.h
+++ debug.h
@@ -3,11 +3,22 @@
 
 #include <QString>
 #include <QDebug>
+#include <QDateTime>
+#include <QFileInfo>
 
 
 #define ASSERT(expr) do { if(!(expr)) { TRACE( QString("ASSERT(%1)").arg(#expr) ); abort(); } } while(0)
 
-#define TRACE(msg) do { qDebug() << (QString(__FILE__) + ":" + QString::number(__LINE__) + ": " + (msg)); } while(0)
+
+#define TRACE(msg) trace(msg, __FILE__, QString::number(__LINE__))
+
+static inline void trace(const QString& f_msg, const QString& f_file, const QString& f_line)
+{
+	QFileInfo f(f_file);
+	qDebug() << (QDateTime::currentDateTime().toString(Qt::DefaultLocaleShortDate) + QString(" @ ") +
+				 f.fileName() + "(" + f_line + ")\t" +
+				 f_msg);
+}
 
 // ============================================================================
 #define NEW_EXCEPT(expr, pParent) \
